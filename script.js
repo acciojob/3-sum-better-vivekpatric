b0fe0210da7e23cc00c8e42b3fd2135b3e3d581d
Vivek Patric R <96299357+vivekpatric@users.noreply.github.com>
2023-05-31 13:37:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function threeSum(arr, target) {
 // write your code here
-  
+	let m=0;
+	let a=[];
+  for(let i=0;i<arr.length;i++){
+	  for(let j=i+1;j<arr.length-1;j++){
+		  for(let k=j+1;k<arr.length-2;k++){
+			  a[m++]=arr[i]+arr[j]+arr[k];
+				  
+			  
+		  }
+	  }
+  }
+	let sum=0;
+	for(let i=0;i<a.length;i++){
+		if(a[i]-target<=sum){
+			sum=arr[i];
+		}
+	}
+	return sum;
 }
 
 module.exports = threeSum;
